a02f8e5ab11b7ca00640c81ec9b2fb76c244c1e0
Alex <aa26_ph@yahoo.com>
2022-02-22 12:53:39 +0800

    initial commit

---
+++ src/main/java/com/iress/eavesrobot/domain/Robot.java
@@ -0,0 +1,6 @@
+package com.iress.eavesrobot.domain;
+
+public class Robot {
+    private Table table;
+
+}
---
+++ src/main/java/com/iress/eavesrobot/domain/Table.java
@@ -0,0 +1,23 @@
+package com.iress.eavesrobot.domain;
+
+public class Table {
+    private int width;
+    private int height;
+
+    public Table(int width, int height) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+
+
+}
